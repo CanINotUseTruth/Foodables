dee303aa3edaf4feedc73eb9478fd94422105470
Truth <tobyrutherfordfb@gmail.com>
2021-07-05 15:06:24 +1000

    V 1.0.5
    The Jerky Update

---
+++ src/main/java/truth/foodables/items/Drink.java
@@ -0,0 +1,24 @@
+package truth.foodables.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.UseAction;
+
+public class Drink extends Item {
+
+    public Drink(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatSound() {
+        return getDrinkSound();
+    }
+    
+}
